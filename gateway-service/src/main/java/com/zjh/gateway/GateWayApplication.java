de242f26fda32ead74d4b12b61faca5380d9f59b
zhaojianhua <1763124707@qq.com>
2024-11-11 18:38:45 +0800

    2024-11-11 调整微服务的框架的依赖版本和模块的名称。

--- gateway-service/src/main/java/com/zjh/gateway/GateWayApplication.java
+++ gateway-service/src/main/java/com/zjh/gateway/GateWayApplication.java
@@ -3,10 +3,15 @@ package com.zjh.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hello world!
+ *
+ */
 @SpringBootApplication
-public class GateWayApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GateWayApplication.class);
+public class GateWayApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(GateWayApplication.class,args);
     }
 }
